fbbeaca88b3fa3894631ffd3b91a335c6892271e
kat-cody <codysgirlie@gmail.com>
2024-04-03 17:48:25 -0400

    fixing

--- knexfile.js
+++ knexfile.js
@@ -2,21 +2,24 @@ const path = require("path");
 
 require("dotenv").config();
 
-//FROM THE ORIGINAL PROJECT FILE
 // const {
 //   DATABASE_URL = "postgresql://postgres@localhost/postgres",
 // } = process.env;
 
-//Attempt to connect with database
 const {
-  DATABASE_URL = "postgres://tbnpnykm:OIt66uuiSzEec0jkwXq_uqzmRhkAb_Xv@castor.db.elephantsql.com/tbnpnykm",
+  NODE_ENV = "development",
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
 } = process.env;
-
+const URL =
+  NODE_ENV === "production"
+    ? PRODUCTION_DATABASE_URL
+    : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -28,7 +31,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
